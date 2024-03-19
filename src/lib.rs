fe791a8951fc15dcd9d9b71698f66e93f0e3bca6
weixubin <weixubin814@gmail.com>
2024-03-19 16:28:45 +0800

    优化 demo

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::error::Error;
+use std::fs;
 mod math;
 
 pub struct Config {
@@ -22,18 +22,17 @@ impl Config {
             Some(arg) => arg,
             None => return Err("Didn't get a file path"),
         };
-        
+
         // 使用 Result 来返回
         Ok(Config { query, file_path })
     }
 }
 
-
 // Box<dyn Error> 特征对象，它表示函数返回一个类型，该类型实现了 Error 特征，这样我们就无需指定具体的错误类型
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // 如果结果是 Ok(T)，则把 T 赋值给 f，如果结果是 Err(E)，则返回该错误，所以 ? 特别适合用来传播错误
     let contents = fs::read_to_string(config.file_path)?;
-   
+
     for line in search(&config.query, &contents) {
         println!("{line}");
     }
@@ -47,16 +46,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         .collect()
 }
 
-
 #[cfg(test)]
 mod type_tests {
+
+    /**
+     * String 与 &str
+     */
     #[test]
     fn test_string() {
         // 右侧是字符串字面量，左侧 a 实际上是 &'static str 类型的值，即指向静态存储区的不可变字符串引用
-        let a= "hello";
+        let a = "hello";
 
         // String::from 方法接收一个字符串字面量，并基于这个字面量创建一个新的、可变的 String 实例。该 String 存储在堆中
-        let b = String::from("hello"); 
+        let b = String::from("hello");
         assert_eq!(b, "hello");
 
         // 所有权转移（move），当值的所有权从 b 转移（move）到 c 后。Rust 会认为 b 不再有效 （注意：如果是字面量则只是拷贝，不会失效）
@@ -70,11 +72,14 @@ mod type_tests {
         change(&mut e);
         assert_eq!(e, "hello, world");
     }
-    fn change(some_string: &mut String){
+    fn change(some_string: &mut String) {
         // 函数没有返回值，那么返回一个 ()
         some_string.push_str(", world")
     }
 
+    /**
+     * 数组 array
+     */
     #[test]
     fn test_array() {
         // 数组存在栈中，一旦创建后大小不能改变
@@ -92,15 +97,40 @@ mod type_tests {
         println!("{:#?}", array);
     }
 
+    /**
+     * 动态数组 vector
+     */
+    #[test]
+    fn test_vector() {
+        // 数组适合于固定长度的场景，而 Vector 则适合于动态长度及存储数据量大的场景，如果容量不足就会触发 vector 扩容
+        let v: Vec<i32> = Vec::new();
+        v.push(1);
+        println!("{:#?}", v);
+
+        // 使用 vec![] 宏创建时可以有初始化值
+        let mut v1 = vec![1, 2, 3];
+        let third: &i32 = &v[2];
+        println!("第三个元素是 {}", third);
+
+        // 基于模式匹配取值
+        match v.get(2) {
+            Some(third) => println!("第三个元素是 {third}"),
+            None => println!("not find！"),
+        }
+    }
+
     // 枚举
 
-    // 动态数组
-
+    /**
+     * 结构体和特征
+     */
     #[test]
     fn test_struct_and_trait() {
-        let post = Post{author: "Sunface".to_string()};
+        let post = Post {
+            author: "Sunface".to_string(),
+        };
         println!("整体信息打印：{:?}", post); // 使用 #[derive(Debug)] 对结构体进行了标记，这样才能使用 println!("{:?}", s); 的方式对其进行打印输出
-        println!("{}",post.summarize());
+        println!("{}", post.summarize());
     }
     pub trait Summary {
         fn summarize(&self) -> String;
@@ -108,7 +138,7 @@ mod type_tests {
     // 结构属性和方法定义是分离的
     #[derive(Debug)]
     pub struct Post {
-        pub author: String
+        pub author: String,
     }
     // 实现特征
     impl Summary for Post {
@@ -119,7 +149,11 @@ mod type_tests {
 }
 
 #[cfg(test)]
-mod ohter_tests {    
+mod ohter_tests {
+
+    /**
+     * 生命周期
+     */
     #[test]
     fn test_lifecycle() {
         let string1 = String::from("long string is long");
@@ -140,14 +174,20 @@ mod ohter_tests {
         }
     }
 
+    /**
+     * package -> crate -> mod  use
+     */
     #[test]
     fn test_mod() {
-         // src/main.rs 和 src/lib.rs 被称为包根(crate root)，这两个文件的内容形成了一个模块 crate 
+        // src/main.rs 和 src/lib.rs 被称为包根(crate root)，这两个文件的内容形成了一个模块 crate
         use crate::math::*; //  也可以使用 super 关键字来表达路径 eg. use super::math::*;
         let result = arithmetic::add(5, 10);
         println!("The sum is: {}", result);
     }
 
+    /**
+     * 函数式编程-闭包
+     */
     #[test]
     fn test_closure() {
         let x = 1;
@@ -162,6 +202,9 @@ mod ohter_tests {
         assert_eq!(1, print_x());
     }
 
+    /**
+     * 智能指针
+     */
     #[test]
     fn test_smart_pointer() {
         // 通过 Box<T> 来创建一个智能指针，Box<T> 是指针，它指向了堆上的数据
